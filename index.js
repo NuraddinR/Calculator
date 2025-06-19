42abbaed3f082686d600c8543a08c3ebdfe83633
NuraddinR <nurusrzayev91@gmail.com>
2025-06-19 14:35:07 +0400

    fix ui

--- index.js
+++ index.js
@@ -33,3 +33,34 @@ btn2.addEventListener("click", () => {
     second.value = "";
     selectOPrt.value = "space";
 })
+
+function renderImages(imgArr) {
+    imgWrapper.textContent = "";
+    imgArr.forEach((image) => {
+      const img = document.createElement("img");
+      img.src = image.src;
+      img.classList.add(image.category);
+      imgWrapper.appendChild(img);  
+    });
+  }
+  
+  renderImages(images);
+  
+  btnAll.addEventListener("click", () => {
+    renderImages(images);
+  });
+  
+  btnNature.addEventListener("click", () => {
+    let nature = images.filter((img) => img.category === "nature");
+    renderImages(nature);
+  });
+  
+  btnAnimal.addEventListener("click", () => {
+    let animal = images.filter((img) => img.category === "animal");
+    renderImages(animal);
+  });
+  
+  btnCity.addEventListener("click", () => {
+    let city = images.filter((img) => img.category === "city");
+    renderImages(city);
+  });
